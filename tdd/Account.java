8fa6dad076151cc5f2af24d374c689f42d9f36d1
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2022-12-12 22:53:21 +0100

    Added TDD app to test my operations

---
+++ tdd/Account.java
@@ -0,0 +1,24 @@
+package tdd;
+
+public class Account {
+    private int balance;
+    public void deposit(int amount) {
+        if (amount >= 0) {
+            balance += amount;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        if (amount < balance && amount >= 0 ) balance -= amount;
+
+//        if (amount >= 0){
+//            balance -= amount;
+//        };
+
+
+    }
+}
