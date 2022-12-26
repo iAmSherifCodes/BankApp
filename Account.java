4f0cb3b82800aba57b5f5ec07f18761291d52149
Sherif Awofiranye <80782493+Classified9@users.noreply.github.com>
2022-12-26 01:30:35 +0100

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,3 +1,4 @@
+package chapter2.BankApp;
 
 public class Account {
 
@@ -29,22 +30,43 @@ public class Account {
         }
     }
 
-    public void withdraw(double cash){
-        if (cash <= 0 ){
-            System.out.println("Invalid withdrawal amount!");
-        }
-
-        if (cash > accountBalance){
-            System.out.println("Insufficient balance! ");
-        }
-        else {
-            accountBalance -= cash ;
+    public void withdraw(double cash, int pin){
+        if (passcode == pin){
+            if (cash <= 0 ){
+                System.out.println("Invalid withdrawal amount!");
+            } else {
+                if (cash > accountBalance){
+                    System.out.println("Insufficient balance! ");
+                } else {
+                    accountBalance -= cash ;
+                }
+            }
+        } else {
+            System.out.println("Incorrect passcode!");
         }
     }
 
 
     public void setName(String name) {
-	  fullName = name ;
+        fullName = name ;
+    }
+
+    public void setPasscode(int passcode) {
+        this.passcode = passcode;
+    }
+
+    public static void transfer(Account userAccount1, double amountToTransfer, Account userAccount2){
+
+        if(userAccount1.getAccountBalance() > amountToTransfer){
+            double user1Balance = userAccount1.getAccountBalance() - amountToTransfer;
+            double user2Balance = userAccount2.getAccountBalance() + amountToTransfer;
+            System.out.printf("%nAccount balance is: %.2f%nAccount1 balance is: %.2f %n", user1Balance, user2Balance);
+        }
+        else {
+            System.out.println("Invalid transfer amount");
+        }
+
+
     }
 
 }
